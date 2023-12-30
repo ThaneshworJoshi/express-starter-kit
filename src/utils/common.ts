3a1e15b31198e8344c442b0b87a8316f3e34cabc
Thaneshwor Joshi <thaneshwor10@gmail.com>
2023-12-30 23:16:36 +0545

    add: implement removeKeysFromObject function to remove specified keys from objects

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,7 @@
+type AnyObject = {
+  [key: string]: any
+}
+
 /**
  @description Retrieves a key from an object that corresponds to a given value in an enumeration.
  @template T The type of the enumeration value (string or number)
@@ -8,4 +12,22 @@
 
 export const getKeyByValue = <T extends string | number>(enumObj: any, enumValue: T): string | undefined => {
   return Object.keys(enumObj).find((key) => enumObj[key] === enumValue)
+}
+
+/**
+ * Removes specified keys from an object
+ * @param obj - The object from which keys should be removed
+ * @param keys - An array of keys to remove from the object
+ * @returns A new object with specified keys removed
+ */
+export const removeKeysFromObject = (obj: AnyObject, keys: string[]): AnyObject => {
+  const newObj = { ...obj }
+
+  keys.forEach((key) => {
+    if (Object.prototype.hasOwnProperty.call(newObj, key)) {
+      delete newObj[key] // Remove the key from the object
+    }
+  })
+
+  return newObj
 }
